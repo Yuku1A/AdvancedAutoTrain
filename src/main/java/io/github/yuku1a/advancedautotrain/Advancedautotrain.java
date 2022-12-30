aa39bef5f1aa512ef3397f9c03fa5837f6ab4cd0
Yuku <54231633+Yuku1A@users.noreply.github.com>
2022-12-31 03:52:43 +0900

    はじめる

---
+++ src/main/java/io/github/yuku1a/advancedautotrain/Advancedautotrain.java
@@ -0,0 +1,22 @@
+package io.github.yuku1a.advancedautotrain;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Advancedautotrain extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

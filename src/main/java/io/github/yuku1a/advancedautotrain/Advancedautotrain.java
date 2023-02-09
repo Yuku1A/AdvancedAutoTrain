93052c9c0d753c12d2a0e6cc5880cdc60af4f545
Yuku <54231633+Yuku1A@users.noreply.github.com>
2023-02-10 05:28:07 +0900

    CStationListTemplateStoreを追加

--- src/main/java/io/github/yuku1a/advancedautotrain/Advancedautotrain.java
+++ src/main/java/io/github/yuku1a/advancedautotrain/Advancedautotrain.java
@@ -1,10 +1,12 @@
 package io.github.yuku1a.advancedautotrain;
 
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
 public final class Advancedautotrain extends JavaPlugin {
     private final SignActionCStation signActionCStation = new SignActionCStation();
+    private CStationListTemplateStore templateStore;
 
     @Override
     public void onLoad() {
@@ -15,11 +17,28 @@ public final class Advancedautotrain extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         SignAction.register(signActionCStation);
+
+        // yamlから戻す場合は登録しないといけないらしい
+        ConfigurationSerialization.registerClass(CStationInfo.class);
+
+        // 登録してから各種ロード
+        templateStore = new CStationListTemplateStore(this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         SignAction.unregister(signActionCStation);
+
+        // 終了前にデータを保存する
+        templateStore.save();
+    }
+
+    /**
+     * CStationListTemplateStoreのプラグインによって管理されるインスタンスを取得します
+     * @return CStationListTemplateStoreのインスタンス
+     */
+    public CStationListTemplateStore getCStationListTemplateStore(){
+        return templateStore;
     }
 }

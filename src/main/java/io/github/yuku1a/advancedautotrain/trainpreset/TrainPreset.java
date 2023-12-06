0fb0a0b883c3bf21f20779994b00cc7a5a359577
Yuku <54231633+Yuku1A@users.noreply.github.com>
2023-12-06 16:54:16 +0900

    ConfigurationSerializableに対応

--- src/main/java/io/github/yuku1a/advancedautotrain/trainpreset/TrainPreset.java
+++ src/main/java/io/github/yuku1a/advancedautotrain/trainpreset/TrainPreset.java
@@ -1,9 +1,14 @@
 package io.github.yuku1a.advancedautotrain.trainpreset;
 
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 列車に適用される設定を表現します。
  */
-public class TrainPreset {
+public class TrainPreset implements ConfigurationSerializable {
     private final String trainName;
     private final String cstationListName;
     private final String routeName;
@@ -30,5 +35,24 @@ public class TrainPreset {
 
     public String getTag() {
         return tag;
+    }
+
+    public static TrainPreset deserialize(Map<String, Object> map) {
+        return new TrainPreset(
+            (String) map.get("trainName"),
+            (String) map.get("cstationListName"),
+            (String) map.get("routeName"),
+            (String) map.get("tag")
+        );
+    }
+
+    @Override
+    public Map<String, Object> serialize() {
+        var map = new HashMap<String, Object>();
+        map.put("trainName", trainName);
+        map.put("cstationListName", cstationListName);
+        map.put("routeName", routeName);
+        map.put("tag", tag);
+        return map;
     }
 }

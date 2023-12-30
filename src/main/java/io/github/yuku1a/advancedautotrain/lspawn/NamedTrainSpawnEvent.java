6ad3cf8e43e428008b7b0f4fd93985d80bffa84f
Yuku <54231633+Yuku1A@users.noreply.github.com>
2023-12-30 19:32:54 +0900

    NamedTrainSpawnEventのファイルにメモを追加

--- src/main/java/io/github/yuku1a/advancedautotrain/lspawn/NamedTrainSpawnEvent.java
+++ src/main/java/io/github/yuku1a/advancedautotrain/lspawn/NamedTrainSpawnEvent.java
@@ -5,6 +5,11 @@ import com.bergerkiller.bukkit.tc.controller.MinecartGroup;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+// スポーンした列車に対してTrainCarts側に保存してない設定などを適用したりするためのフック
+// スポーンした列車の検出方法などとは独立した仕様
+// これ系のコードの結果としてTrainCarts側で設定する情報の量を減らすことができる
+// TrainCarts内で列車に大量の設定を投入するのはかなり面倒くさいのでその軽減策
+// 現状の実装ではセーブされた列車にしてセーブする名前とは別に名前を設定する必要がある
 public class NamedTrainSpawnEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
     @Override

976ca96a78caf9f7d324e67f9a3d2406e0d70b66
Yuku <54231633+Yuku1A@users.noreply.github.com>
2024-01-21 17:48:51 +0900

    LSpawnSignManagerの登録および削除処理を追加

--- src/main/java/io/github/yuku1a/advancedautotrain/lspawn/LSpawnSignManager.java
+++ src/main/java/io/github/yuku1a/advancedautotrain/lspawn/LSpawnSignManager.java
@@ -1,6 +1,7 @@
 package io.github.yuku1a.advancedautotrain.lspawn;
 
 import com.bergerkiller.bukkit.common.Task;
+import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.offline.sign.OfflineSign;
 import com.bergerkiller.bukkit.tc.offline.sign.OfflineSignMetadataHandler;
 import com.bergerkiller.bukkit.tc.offline.sign.OfflineSignSide;
@@ -69,6 +70,40 @@ public class LSpawnSignManager {
     public void disable() {
         this.plugin.getTrainCarts().getOfflineSigns().unregisterHandler(LSpawnSignMetadata.class);
         this.task.stop();
+    }
+
+    public boolean register(SignActionEvent event) {
+        // nullチェック
+        if (event == null)
+            return false;
+
+        // 実際に置かれている看板じゃない場合蹴る
+        if (!event.getTrackedSign().isRealSign())
+            return false;
+
+        // すでに登録されているかどうか確認する
+        var signside = OfflineSignSide.of(event.getTrackedSign());
+        var stored = spawnSignList.get(signside);
+
+        if (stored != null){
+            // すでに登録されている場合、verifysignで更新ができる
+            plugin.getTrainCarts().getOfflineSigns().verifySign(event.getSign(), signside.isFrontText(), LSpawnSignMetadata.class);
+        }
+
+        // 新規登録の場合の処理
+        var metadata = new LSpawnSignMetadata(true);
+
+        // OfflineSignStoreに登録
+        plugin.getTrainCarts().getOfflineSigns().put(event.getTrackedSign(), metadata);
+
+        // ↑に登録してあるハンドラー経由でこっちにも自動的に登録されてるはずだから確認
+        if (spawnSignList.get(signside) != null)
+            return true;
+        return false;
+    }
+
+    public void unregister(SignActionEvent event) {
+        plugin.getTrainCarts().getOfflineSigns().remove(event.getTrackedSign(), LSpawnSignMetadata.class);
     }
 
     private Task newTask() {

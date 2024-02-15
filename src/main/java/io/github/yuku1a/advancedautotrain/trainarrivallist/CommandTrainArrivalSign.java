639312302637cdd77ce3de55b20e1614cef48950
Yuku <54231633+Yuku1A@users.noreply.github.com>
2024-02-16 03:19:48 +0900

    CStationの名前を含めて生成するように変更、コードも調整

--- src/main/java/io/github/yuku1a/advancedautotrain/trainarrivallist/CommandTrainArrivalSign.java
+++ src/main/java/io/github/yuku1a/advancedautotrain/trainarrivallist/CommandTrainArrivalSign.java
@@ -13,21 +13,21 @@ public class CommandTrainArrivalSign implements CommandExecutor {
     // addコマンド
     private boolean add(CommandSender sender, String[] args) {
         // 引数のヘルプ用のテキスト
-        var argtext = "<list> <displayname> [offset] [description]";
+        var argtext = "<list> <displayname> <cstationname> [offset] [description]";
 
         var command = args[0];
 
         if (command.equals("add")) {
-            // コマンド指定で1つ、リスト指定で1つ、パラメータが1つ、オプション2つで5つ
-            if ((3 > args.length) || (args.length > 5)) {
+            // コマンド指定で1つ、リスト指定で1つ、パラメータが2つ、オプション2つで6つ
+            if ((4 > args.length) || (args.length > 6)) {
                 return commandsHelp(
                     sender,
                     "add " + argtext
                 );
             }
         } else if(command.equals("replace") || command.equals("insert")) {
-            // コマンド指定で1つ、リスト指定で1つ、パラメータが1つ、オプション2つ、インデックス1つで6つ
-            if ((4 > args.length) || (args.length > 6)) {
+            // コマンド指定で1つ、リスト指定で1つ、パラメータが2つ、オプション2つ、インデックス1つで7つ
+            if ((5 > args.length) || (args.length > 7)) {
                 return commandsHelp(
                     sender,
                     args[0] + " " + argtext + " <index>"
@@ -40,13 +40,14 @@ public class CommandTrainArrivalSign implements CommandExecutor {
         // リスト指定
         var key = args[1];
         // リストを取ってくる
-        var list = listOrNull(sender, args[1]);
+        var list = listOrNull(sender, key);
         // nullだったらメッセージが出てるのでそのまま返す
         if (list == null)
             return true;
 
         // 取り出し、型変換
         var displayname = args[2];
+        var cstationname = args[3];
 
         // displayname以外がオプションであるかインデックスなので慎重に調べる
         long offset;
@@ -54,46 +55,48 @@ public class CommandTrainArrivalSign implements CommandExecutor {
         String description;
 
         // オプションがない場合
-        if ((command.equals("add") && args.length == 3) || (!(command.equals("add"))) && args.length == 4) {
+        if ((command.equals("add") && args.length == 4) || (!(command.equals("add"))) && args.length == 5) {
             offset = defaultoffset;
             description = null;
         } else {
-            // オプションがあるパターンだとindex抜きの引数がいくつあるか計算する
+            // 最後の判定に必要
+            // addコマンド以外だとindexがあるのでargs.lengthでは判定できない
+            // オプションなしが4
             int argnum;
             if (command.equals("add"))
                 argnum = args.length;
             else
                 argnum = args.length - 1;
 
+            // インデックスを進めていってあれこれ
+            int argindex = 4;
+
             // OffSetを取り出す
             long argoffset;
-            boolean arg3IsOffset;
             try {
-                argoffset = Long.parseLong(args[3]);
-                arg3IsOffset = true;
+                argoffset = Long.parseLong(args[argindex]);
+                argindex++;
             } catch (NumberFormatException e) {
-                if (argnum == 5) {
-                    sender.sendMessage("オフセットの値が不正です。");
-                    return true;
-                }
                 argoffset = defaultoffset;
-                arg3IsOffset = false;
             }
             offset = argoffset;
 
-            // コメントを書きたくない
-            if (arg3IsOffset){
-                if (argnum == 4)
-                    description = null;
-                else
-                    description = args[4];
-            }
-            else {
-                description = args[3];
+            // descriptionを取り出す
+            // 全部乗せだったらindexは5
+            if (argindex < argnum){
+                description = args[argindex];
+                argindex++;
+            } else
+                description = null;
+
+            // 正しければこれはelse側を通る
+            if (argindex < argnum) {
+                sender.sendMessage("引数が不正です");
+                return true;
             }
         }
 
-        var entry = new ArrivalSignEntry(displayname, offset, description);
+        var entry = new ArrivalSignEntry(cstationname, displayname, offset, description);
 
         // add以外だとindexが存在するので読み取る
         int index;

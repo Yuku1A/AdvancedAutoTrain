e5a96e4884af9055f5ee5cce9f3ca584bd2ec45f
Yuku <54231633+Yuku1A@users.noreply.github.com>
2024-08-09 17:47:45 +0900

    保存を忘れてた

--- src/main/java/io/github/yuku1a/advancedautotrain/Advancedautotrain.java
+++ src/main/java/io/github/yuku1a/advancedautotrain/Advancedautotrain.java
@@ -217,6 +217,7 @@ public final class Advancedautotrain extends JavaPlugin {
         propreg.unregister(cStationListProperty);
         // 終了前にデータを保存する
         templateStore.save();
+        cStationCacheSet.save();
 
         // OPTimer絡みの処理
         operationTimerStore.freeze();
